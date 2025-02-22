cb745c7521be46a82a254b71dda6a35a4df46685
Apavayan Sinha <info@apavayan.com>
2025-02-22 16:13:14 +0530

    feat: integrate Tailwind CSS and update App component layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
-
-
 export default function App() {
-  return (
-    <div>App</div>
-  )
+    return (
+        <div className="flex max-h-screen w-full">
+            <h1 className="text-3xl font-bold underline">Hello world!</h1>
+        </div>
+    );
 }
