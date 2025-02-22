84dd96b8b931525d8150cd180629b5f4a9a9b01f
Apavayan Sinha <info@apavayan.com>
2025-02-22 17:08:32 +0530

    stagging: add ipod buttons icons

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,40 @@
+import {
+  PauseIcon,
+  PlayIcon,
+  SkipBackIcon,
+  SkipForwardIcon,
+} from "lucide-react";
+
 export default function App() {
+  const buttonSize = 24;
   return (
     // TODO: animate background color change
     <div className="flex min-h-screen w-full items-center justify-center">
-      <div className="flex flex-col bg-gray-200 h-96 w-64 rounded-2xl border border-gray-300 p-4 items-center">
-        <div className="flex bg-white min-h-40 w-full rounded-lg">
+      <div className="flex flex-col bg-gray-200 h-full w-64 rounded-2xl border border-gray-300 p-4 items-center">
+        <div className="flex bg-white min-h-44 w-full rounded-lg p-0 m-0">
           {/* TODO: Add Display Comp Contralled by Controller */}
           <h1 className="text-2xl font-bold">Hello World</h1>
         </div>
         <div className="mt-4">
           {/* TODO: add Controller to Navigate */}
-          <div className="flex items-center justify-center bg-gray-300 h-40 w-40 rounded-full">
-            <div className="bg-gray-200 h-20 w-20 rounded-full"></div>
+          <div className="relative bg-gray-300 h-40 w-40 rounded-full text-gray-700 broder border-gray-400 max-h-56 cursor-pointer">
+            <div className="absolute top-0 left-1/2 transform -translate-x-1/2 translate-y-3">
+              <h2 className="uppercase font-sans font-bold text-sm">MENU</h2>
+            </div>
+            <SkipBackIcon
+              size={buttonSize}
+              className="absolute top-1/2 left-0 transform translate-x-2 -translate-y-1/2"
+            />
+            <SkipForwardIcon
+              size={buttonSize}
+              className="absolute top-1/2 right-0 transform -translate-x-2 -translate-y-1/2"
+            />
+            <div className="flex absolute bottom-0 left-1/2 transform -translate-x-1/2 -translate-y-3">
+              <PlayIcon size={buttonSize - 2} />
+              <PauseIcon size={buttonSize - 2} />
+            </div>
+            {/* Black Space in center */}
+            <div className="absolute top-[25%] left-[25%] bg-gray-200 min-h-20 min-w-20 rounded-full cursor-default border border-gray-400/50"></div>
           </div>
         </div>
       </div>
