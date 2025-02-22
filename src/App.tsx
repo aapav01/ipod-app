51741e0208a62c2dc422ef2fca259ad570b13258
Apavayan Sinha <info@apavayan.com>
2025-02-22 16:32:07 +0530

    stagging: add basic layout for the Ipod Player

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,19 @@
 export default function App() {
-    return (
-        <div className="flex max-h-screen w-full">
-            <h1 className="text-3xl font-bold underline">Hello world!</h1>
+  return (
+    // TODO: animate background color change
+    <div className="flex min-h-screen w-full items-center justify-center">
+      <div className="flex flex-col bg-gray-200 h-96 w-64 rounded-2xl border border-gray-300 p-4 items-center">
+        <div className="flex bg-white min-h-40 w-full rounded-lg">
+          {/* TODO: Add Display Comp Contralled by Controller */}
+          <h1 className="text-2xl font-bold">Hello World</h1>
         </div>
-    );
+        <div className="mt-4">
+          {/* TODO: add Controller to Navigate */}
+          <div className="flex items-center justify-center bg-gray-300 h-40 w-40 rounded-full">
+            <div className="bg-gray-200 h-20 w-20 rounded-full"></div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
