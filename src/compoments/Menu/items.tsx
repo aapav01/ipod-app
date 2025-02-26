503c4c45a24298ca59ba1832eb11cded0af07efa
Apavayan Sinha <info@apavayan.com>
2025-02-26 09:22:02 +0530

    chore: define menu items and their corresponding screens.

--- src/compoments/Menu/items.tsx
+++ src/compoments/Menu/items.tsx
@@ -2,13 +2,21 @@ import React from "react";
 import Settings from "../LeafScreen/Settings";
 import Games from "../LeafScreen/Games";
 
-/**
- * TODO: Add ReactNode type to the menuScreens object
- * @type {Object.<string, string[]>}
- */
-const menuScreens: {
+interface MenuScreens {
   [key: string]: string[] | React.ReactNode;
-} = {
+}
+
+/**
+ * Menu screens configuration
+ *
+ * @type {MenuScreens}
+ * @property {string[]} main - Main menu items
+ * @property {string[]} music - Music menu items
+ * @property {string[]} settings - Settings menu items
+ * @property {string[]} games - Games menu items
+ */
+
+const menuScreens: MenuScreens = {
   main: ["Music", "Games", "Settings"],
   music: ["All songs", "Artists", "Albums", "Playlists"],
   settings: <Settings />,
