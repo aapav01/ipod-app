a55e7096917ffbadbe6f8f0474f4cccc187a9f0f
Apavayan Sinha <info@apavayan.com>
2025-02-26 09:29:05 +0530

    fix: add LeafScreen component to handle invalid submenu types

--- src/compoments/LeafScreen/index.tsx
+++ src/compoments/LeafScreen/index.tsx
@@ -1,10 +1,4 @@
-/**
- * LeafScreen component
- * @param {string[] | React.ReactNode} submenus
- * @param {number} selectedIndex
- * @returns ReactElement
- */
-
+import React from "react";
 import Menu from "../Menu";
 
 type LeafScreenProps = {
@@ -12,11 +6,19 @@ type LeafScreenProps = {
   selectedIndex: number;
 };
 
+/**
+ * LeafScreen component
+ * @param {string[] | React.ReactNode} submenus
+ * @param {number} selectedIndex
+ * @returns ReactElement
+ */
 function LeafScreen({ submenus, selectedIndex }: LeafScreenProps) {
   if (Array.isArray(submenus)) {
     return <Menu items={submenus} selectedIndex={selectedIndex} />;
-  } else {
+  } else if (React.isValidElement(submenus)) {
     return submenus;
+  } else {
+    return <div>No content available.</div>;
   }
 }
 
